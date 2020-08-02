4df4c9e577659929dc284247f8e31d260766584f
unknown <gauravs260668@gmail.com>
2020-08-02 09:57:52 +0530

    updated

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:authentication/pages/root_page.dart';
+import 'package:authentication/services/authentication.dart';
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
